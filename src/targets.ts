3b1c2a305c4dca9a449a4f2cdc425a900cc950d9
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-07-30 08:30:40 +0200

    windows: shorten conan cache path (#43)
    * windows: shorten conan cache path
    
    also, add a job summary to find out what is build built
    
    * downgrade to macos-11
    
    conan has no binaries available for macos-latest
    
    * use build=missing
    
    build=outdated does not exist in conan 2

--- src/targets.ts
+++ src/targets.ts
@@ -1,38 +1,38 @@
 export default [
   {
     os: "ubuntu-latest",
-    cmd: "--build=outdated",
+    cmd: "--build=missing",
     enabled: true,
   },
   {
     os: "windows-latest",
-    cmd: "--build=outdated",
+    cmd: "--build=missing",
     enabled: true,
   },
   {
     os: "windows-latest",
     toolchain: "mingw-builds/11.2.0",
-    cmd: "--build=outdated",
+    cmd: "--build=missing",
     enabled: true,
   },
   {
     os: "ubuntu-latest",
-    cmd: "--build=outdated -s compiler.libcxx=libstdc++11",
+    cmd: "--build=missing -s compiler.libcxx=libstdc++11",
     enabled: true,
   },
   {
-    os: "macos-latest",
-    cmd: "--build=outdated",
+    os: "macos-11",
+    cmd: "--build=missing",
     enabled: true,
   },
   {
     os: "ubuntu-latest",
     container: "conanio/gcc9-armv7hf",
-    cmd: "--build=outdated -s:h arch=armv7hf",
+    cmd: "--build=missing -s:h arch=armv7hf",
   },
   {
     os: "ubuntu-latest",
     container: "conanio/gcc9",
-    cmd: "--build=outdated",
+    cmd: "--build=missing",
   },
 ];
