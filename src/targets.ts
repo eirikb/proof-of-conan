291a821aced88ea04e3ff1f8375090a747d1f483
ericLemanissier <ericLemanissier@users.noreply.github.com>
2026-03-09 09:49:32 +0100

    use build=compatible
    also, migrate to macos-latest

--- src/targets.ts
+++ src/targets.ts
@@ -1,38 +1,38 @@
 export default [
   {
     os: "ubuntu-latest",
-    cmd: "--build=missing",
+    cmd: "--build=compatible",
     enabled: true,
   },
   {
     os: "windows-latest",
-    cmd: "--build=missing",
+    cmd: "--build=compatible",
     enabled: true,
   },
   {
     os: "windows-latest",
     toolchain: "mingw-builds/11.2.0",
-    cmd: "--build=missing",
+    cmd: "--build=compatible",
     enabled: true,
   },
   {
     os: "ubuntu-latest",
-    cmd: "--build=missing -s compiler.libcxx=libstdc++11",
+    cmd: "--build=compatible -s compiler.libcxx=libstdc++11",
     enabled: true,
   },
   {
-    os: "macos-11",
-    cmd: "--build=missing",
+    os: "macos-latest",
+    cmd: "--build=compatible",
     enabled: true,
   },
   {
     os: "ubuntu-latest",
     container: "conanio/gcc9-armv7hf",
-    cmd: "--build=missing -s:h arch=armv7hf",
+    cmd: "--build=compatible -s:h arch=armv7hf",
   },
   {
     os: "ubuntu-latest",
     container: "conanio/gcc9",
-    cmd: "--build=missing",
+    cmd: "--build=compatible",
   },
 ];
